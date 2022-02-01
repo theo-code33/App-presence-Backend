cbcfc9016777234c314801ca393b0bc98065deab
Théo Gillet <gillettheo1@gmail.com>
2022-02-01 17:05:35 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const cors = require('cors')
+var bodyParser = require("body-parser");
+const { request } = require('express');
+const app = express()
+const port = 3000
+
+app.use(cors())
+app.use(bodyParser.json())
+
+const students =[]
+
+app.post('/', (request, response) => {
+    console.log(request.body)
+    students.push(request.body)
+})
+
+app.get('http://localhost:4200', (request, response) => {
+    response.send(students)
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
