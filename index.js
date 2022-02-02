d770d77118a4ee98f5fec9cb90226deb4a2f1542
Théo Gillet <gillettheo1@gmail.com>
2022-02-02 19:29:38 +0100

    add post of student id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 var bodyParser = require("body-parser");
-const { request } = require('express');
+const { request, response } = require('express');
 const app = express()
 const port = 3000
 
@@ -11,14 +11,22 @@ app.use(bodyParser.json())
 const students =[]
 
 app.post('/', (request, response) => {
+    const id = students.length
     console.log(request.body)
     students.push(request.body)
-    response.send()
+    console.log("User id " + id + " Created ");
+    response.send({id})
 })
 
-app.get('http://localhost:4200', (request, response) => {
+app.get('/student-by-id/:id', (request,response) => {
+    const id = request.params.id
+    response.send(students[id]);
+})
+
+app.get('/students', (request, response) => {
     response.send(students)
 })
+
 
 
 app.listen(port, () => {
