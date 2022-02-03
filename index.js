5223bcc2d2e08d085fad295cf6cd8a135be17c16
Théo Gillet <gillettheo1@gmail.com>
2022-02-03 16:54:20 +0100

    add timer

--- index.js
+++ index.js
@@ -10,12 +10,36 @@ app.use(bodyParser.json())
 
 const students =[]
 
+
+const stop = setInterval(() => {
+    const myDate = new Date()
+    const timeRest = new Date()
+    timeRest.setHours(timeRest.getHours() + 1)
+    timeRest.setMinutes(0)
+    timeRest.setSeconds(0)
+      countDown = timeRest - myDate
+      countDown = countDown / 1000
+    if(countDown <= 1){
+        while(students.length > 0){
+            students.pop()
+        }
+    }
+}, 1000)
+
+
 app.post('/', (request, response) => {
     const id = students.length
     console.log(request.body)
     students.push(request.body)
     console.log("User id " + id + " Created ");
     response.send({id})
+})
+
+app.get('/class/:id', (request, response) => {
+    const userId = request.params.id
+    const studentClass = students[userId].class
+    const result = students.filter(students => studentClass == students.class)
+    response.send(result)
 })
 
 app.get('/student-by-id/:id', (request,response) => {
